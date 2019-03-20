dbdd1116a0cd5000b15ace7f7ee8e01c452ef95f
Diol <diolr23@mail.ru>
2019-03-20 09:07:18 +0300

    added patch file and source code for task 2

--- 1-3/README.md
+++ 1-3/README.md
@@ -2,6 +2,10 @@
 
 task01-03-01.exe_ - keygen for CRACKME-31158-5470e7.EXE
 task01-03-01.c - keygen source code
+task01-03-01p.exe_ - patch for CRACKME-31158-5470e7.EXE
+task01-03-01p.c - patch source code
 
-task01-03-02.exe_ - keygen for Otus_Crackme_01-31158-03a357.EXE
+task01-03-02.exe_ - keygen for Otus_Crackme_01-31158-03a357.exe
 task01-03-02.c - keygen source code
+task01-03-02p.exe_ - patch for Otus_Crackme_01-31158-03a357.exe
+task01-03-02p.c - patch source code
--- 1-3/task01-03-02p.c
+++ 1-3/task01-03-02p.c
@@ -8,19 +8,22 @@ int main(int argc, char* argv[])
 {
 	FILE * pFile;
 	if(argc == 1) {
-    	printf("task01-03-01p.exe file_to_patch\n");
+    	printf("task01-03-02p.exe file_to_patch\n");
 		system("pause");
 		return EXIT_SUCCESS;
 	}
 	pFile = fopen(argv[1], "r+b");
 
 	if (pFile != NULL) {
-		fseek(pFile, 0x841, SEEK_SET);
-		unsigned char newByte1 = 0x39;
+		fseek(pFile, 0x72c7, SEEK_SET);
+		unsigned char newByte1 = 0x90;
 		fwrite( &newByte1, sizeof( newByte1 ), 1, pFile );
-		fseek(pFile, 0x842, SEEK_SET);
-		unsigned char newByte2 = 0xC0;
+		fseek(pFile, 0x72c8, SEEK_SET);
+		unsigned char newByte2 = 0x39;
 		fwrite( &newByte2, sizeof( newByte2 ), 1, pFile );
+		fseek(pFile, 0x72c9, SEEK_SET);
+		unsigned char newByte3 = 0xc0;
+		fwrite( &newByte3, sizeof( newByte3 ), 1, pFile );
 		fclose(pFile);
 		printf("File Patched Successfully.\n");
 	}
